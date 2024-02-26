bc72261ea58029b21789f426a4c2503ec54344be
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-26 17:18:55 -0300

    1.4. Builder

---
+++ builder/pedido-venda/src/main/java/com/algaworks/model/builder/PedidoVendaBuilder.java
@@ -0,0 +1,56 @@
+package com.algaworks.model.builder;
+
+import com.algaworks.model.Cliente;
+import com.algaworks.model.ItemPedido;
+import com.algaworks.model.PedidoVenda;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class PedidoVendaBuilder {
+
+  private PedidoVenda instancia;
+
+  public PedidoVendaBuilder(){
+    this.instancia = new PedidoVenda();
+  }
+
+  public PedidoVenda construir(){
+    return this.instancia;
+  }
+
+  public PedidoVendaBuilder comClienteVip(String nome){
+    definirCliente(nome, true);
+    return this;
+  }
+
+  public PedidoVendaBuilder comClienteNormal(String nome){
+    definirCliente(nome, false);
+    return this;
+  }
+
+  public PedidoVendaBuilder comItem(String nome, Integer quantidade, BigDecimal valor){
+    ItemPedido itemPedido = new ItemPedido();
+    itemPedido.setNome(nome);
+    itemPedido.setQuantidade(quantidade);
+    itemPedido.setValorUnitario(valor);
+
+    if(this.instancia.getItensPedido() == null){
+      this.instancia.setItensPedido(new ArrayList<ItemPedido>());
+    }
+
+    this.instancia.getItensPedido().add(itemPedido);
+
+    return this;
+
+  }
+
+  private void definirCliente(String nome, boolean b) {
+    Cliente cliente = new Cliente();
+    cliente.setNome(nome);
+    cliente.setVip(true);
+    this.instancia.setCliente(cliente);
+  }
+
+
+}
